9c44387df5d70597ab7075b78a92d68782efe142
kiplrry <kiplarry19@gmail.com>
2023-11-29 15:16:41 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 #Puppet code to install a package flask v2.1.0
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip'],
 }
