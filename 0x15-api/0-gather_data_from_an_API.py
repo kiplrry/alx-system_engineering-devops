9be5ec9765e4e5ce4f8c6b4540d4c9b4c9423bad
kiplrry <kiplarry19@gmail.com>
2024-02-19 17:24:51 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,21 +5,21 @@ returns information about his/her TODO list progress.
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    num = argv[1]
+    r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={num}')
+    name = requests.get(f"""https://jsonplaceholder.typicode.com/users/{num}""")\
+        .json()['name']
 
-num = argv[1]
-r = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={num}')
-name = requests.get(f"""https://jsonplaceholder.typicode.com/users/{num}""")\
-    .json()['name']
+    rjson = r.json()
+    alltodos = len(rjson)
+    donetodos = 0
+    todos = ""
+    for todo in rjson:
+        if todo["completed"] is True:
+            donetodos += 1
+        todos += f"\n\t {todo['title']}"
 
-rjson = r.json()
-alltodos = len(rjson)
-donetodos = 0
-todos = ""
-for todo in rjson:
-    if todo["completed"] is True:
-        donetodos += 1
-    todos += f"\n\t {todo['title']}"
-
-statement = f"Employee {name} is done with tasks({donetodos}/{alltodos}):\
-{todos}"
-print(statement)
+    statement = f"Employee {name} is done with tasks({donetodos}/{alltodos}):\
+    {todos}"
+    print(statement)
