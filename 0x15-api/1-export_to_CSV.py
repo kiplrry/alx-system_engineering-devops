690cccdc08d6a147e73f423984399c10b28b094d
kiplrry <kiplarry19@gmail.com>
2024-02-19 18:56:18 +0300

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    num = argv[1]
+    r = requests.get(
+        f'https://jsonplaceholder.typicode.com//users/{num}/todos'
+        )
+    name = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{num}"
+                        ).json()['name']
+
+    rjson = r.json()
+
+    with open(f"{num}.csv", "w", encoding="utf-8") as fp:
+        for todo in rjson:
+            string = (f"\"{todo['id']}\",\"{name}\","
+                      f"\"{todo['completed']}\",\"{todo['title']}\"\n")
+            fp.write(string)
