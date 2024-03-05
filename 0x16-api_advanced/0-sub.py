ea4257bbb5be5a96f6b06f200269c934cf350111
kiplrry <kiplarry19@gmail.com>
2024-03-05 23:31:44 +0300

    0-sub.py

---
+++ 0x16-api_advanced/0-sub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""returns number of subscribers for a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
+
+    headers = {'User-Agent': 'Google Chrome Version 110.0.5481.105'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()
+        return data['data']['subscribers']
+    else:
+        return 0
