8d4e52c6cbfeac08c5aecad7b8e2f7f08e6af5e4
kiplrry <kiplarry19@gmail.com>
2024-03-05 23:37:12 +0300

    0-subs.py.fixx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,8 @@ def number_of_subscribers(subreddit):
 
     headers = {'User-Agent': 'Google Chrome Version 110.0.5481.105'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, timeout=1000)
     if res.status_code == 200:
         data = res.json()
         return data['data']['subscribers']
-    else:
-        return 0
+    return 0
