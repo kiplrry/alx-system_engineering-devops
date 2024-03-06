6a72fb4e8a333f404277c12a2f0bfc09136caac6
kiplrry <kiplarry19@gmail.com>
2024-03-06 18:59:10 +0300

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+# A puppet manuscript to fix an apache server
+# It repaces a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
